0231391bc3f014da6e5d81e8dfa0cc09d6e8a955
YashuDaKJ <aruntailor635@gmail.com>
2026-09-09 14:25:31 +0530

    Create actions_data.py with ACTION_GIFS dictionary placeholder

---
+++ actions_data.py
@@ -0,0 +1,16 @@
+# Action GIFs Dictionary for FlamingDeath Roleplay Commands
+# Placeholder GIFs will be replaced with actual links later
+
+ACTION_GIFS = {
+    'hug': ["https://placeholder.com/gif.gif"],
+    'punch': ["https://placeholder.com/gif.gif"],
+    'pat': ["https://placeholder.com/gif.gif"],
+    'slap': ["https://placeholder.com/gif.gif"],
+    'doom': ["https://placeholder.com/gif.gif"],
+    'burn': ["https://placeholder.com/gif.gif"],
+    'blast': ["https://placeholder.com/gif.gif"],
+    'highfive': ["https://placeholder.com/gif.gif"],
+    'cake': ["https://placeholder.com/gif.gif"],
+    'spray': ["https://placeholder.com/gif.gif"],
+    'pie': ["https://placeholder.com/gif.gif"],
+}
