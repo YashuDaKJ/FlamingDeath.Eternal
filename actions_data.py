f2c486fdc25ed841287a7207decd8897c0785d1e
YashuDaKJ <aruntailor635@gmail.com>
2026-09-09 14:33:46 +0530

    Complete actions_data.py with all 110 GIFs for 11 commands (10 GIFs each)

--- actions_data.py
+++ actions_data.py
@@ -1,5 +1,5 @@
 # Action GIFs Dictionary for FlamingDeath Roleplay Commands
-# Part 1: First 5 Commands (50 GIFs)
+# Complete with all 110 GIFs across 11 commands
 
 ACTION_GIFS = {
     "hug": [
@@ -63,21 +63,75 @@ ACTION_GIFS = {
         "https://media.giphy.com/media/109mX58yPz2328/giphy.gif"
     ],
     "burn": [
-        "https://placeholder.com/gif.gif"
+        "https://media.giphy.com/media/xU1spRleSWalU2fX60/giphy.gif",
+        "https://media.giphy.com/media/P7JmDW7IkB7TW/giphy.gif",
+        "https://media.giphy.com/media/3o72FfM5HJydzaM6B2/giphy.gif",
+        "https://media.giphy.com/media/5nsiFjdgylfK3csZ5E/giphy.gif",
+        "https://media.giphy.com/media/Lopx9eUi34rbq/giphy.gif",
+        "https://media.giphy.com/media/nrXif4YjgX9sI/giphy.gif",
+        "https://media.giphy.com/media/iH2IldJRghWXNEJ43C/giphy.gif",
+        "https://media.giphy.com/media/YoB1eEFB6FZv4yCiFi/giphy.gif",
+        "https://media.giphy.com/media/wA6a5O3I7Xrq8/giphy.gif",
+        "https://media.giphy.com/media/EUnAbl8B91OFi/giphy.gif"
     ],
     "blast": [
-        "https://placeholder.com/gif.gif"
+        "https://media.giphy.com/media/ceHKRKMR6Ojao/giphy.gif",
+        "https://media.giphy.com/media/oe3333B3X85BS/giphy.gif",
+        "https://media.giphy.com/media/XUFPGrX5Zis6Y/giphy.gif",
+        "https://media.giphy.com/media/3o6Ztn39rB7S3mR2cE/giphy.gif",
+        "https://media.giphy.com/media/xT9Igk31LOfiP35q2A/giphy.gif",
+        "https://media.giphy.com/media/eK12uCsrVKxA4/giphy.gif",
+        "https://media.giphy.com/media/wA6a5O3I7Xrq8/giphy.gif",
+        "https://media.giphy.com/media/3o7TKL843E6iM3L5nO/giphy.gif",
+        "https://media.giphy.com/media/AThUu38AmeZxL2S2qU/giphy.gif",
+        "https://media.giphy.com/media/3pLOOnO1lB5XG/giphy.gif"
     ],
     "highfive": [
-        "https://placeholder.com/gif.gif"
+        "https://media.giphy.com/media/aD1fI3UU638yY/giphy.gif",
+        "https://media.giphy.com/media/3M4NpbLCTxBqU/giphy.gif",
+        "https://media.giphy.com/media/G3va3f9E3A9vu/giphy.gif",
+        "https://media.giphy.com/media/wnm0fL2M2O69q/giphy.gif",
+        "https://media.giphy.com/media/sv2KVORze4t2w/giphy.gif",
+        "https://media.giphy.com/media/K9IbsI180Q8py/giphy.gif",
+        "https://media.giphy.com/media/l2QDM9Jnim1YV55YA/giphy.gif",
+        "https://media.giphy.com/media/109A5G32DYSRqg/giphy.gif",
+        "https://media.giphy.com/media/od5H3LnMhdWOoXZnGZ/giphy.gif",
+        "https://media.giphy.com/media/u9BxFE6NoG0es/giphy.gif"
     ],
     "cake": [
-        "https://placeholder.com/gif.gif"
+        "https://media.giphy.com/media/13n8txR8c9Z3N6/giphy.gif",
+        "https://media.giphy.com/media/cK8aBwW7hX78c/giphy.gif",
+        "https://media.giphy.com/media/xT0xeQ9HQw8Pj46Ebm/giphy.gif",
+        "https://media.giphy.com/media/26ufc0OcE3C6wTzRS/giphy.gif",
+        "https://media.giphy.com/media/3oEhmI1YziLzX0mRkk/giphy.gif",
+        "https://media.giphy.com/media/LwK4O2JzTIfxK/giphy.gif",
+        "https://media.giphy.com/media/3o7WTDH9gYo71TurPq/giphy.gif",
+        "https://media.giphy.com/media/5wWf7HapU72P203T3X4/giphy.gif",
+        "https://media.giphy.com/media/l0HlJ764Cq3gC9Zyo/giphy.gif",
+        "https://media.giphy.com/media/xUPGcMZwkJ2XWb020U/giphy.gif"
     ],
     "spray": [
-        "https://placeholder.com/gif.gif"
+        "https://media.giphy.com/media/3oKIPoZniJ2hq8IItG/giphy.gif",
+        "https://media.giphy.com/media/l0HlGVD6zwwK768nu/giphy.gif",
+        "https://media.giphy.com/media/3o7aD2saal6gCGWYAQ/giphy.gif",
+        "https://media.giphy.com/media/xUPGcq17EmWAmSYzxK/giphy.gif",
+        "https://media.giphy.com/media/26FPCXdkvDbKBbgOI/giphy.gif",
+        "https://media.giphy.com/media/3oriNU06XSVW7ow3T2/giphy.gif",
+        "https://media.giphy.com/media/l41YkFIiBxQdRlKI8/giphy.gif",
+        "https://media.giphy.com/media/3o7aTskHEU61D02Ahy/giphy.gif",
+        "https://media.giphy.com/media/xT4uQaz24xgfsLE0kU/giphy.gif",
+        "https://media.giphy.com/media/l2JHQh6O5Qh2cIfM4/giphy.gif"
     ],
     "pie": [
-        "https://placeholder.com/gif.gif"
+        "https://media.giphy.com/media/3o6Zt6bK1wD59a8u2Q/giphy.gif",
+        "https://media.giphy.com/media/l2Sqe1E42QC7BTYWY/giphy.gif",
+        "https://media.giphy.com/media/26FPO1Z6gK9K0oMDK/giphy.gif",
+        "https://media.giphy.com/media/3oriOaeTzC2o3O15n2/giphy.gif",
+        "https://media.giphy.com/media/xT0BKhz4h9wHqM9LUI/giphy.gif",
+        "https://media.giphy.com/media/l0MYKz5r0K0V0pXoI/giphy.gif",
+        "https://media.giphy.com/media/3oKIPzO6I0Y6sVb7uE/giphy.gif",
+        "https://media.giphy.com/media/26BGIqUMlwcqwFPUY/giphy.gif",
+        "https://media.giphy.com/media/3o7aDdz5c5U0sR5EaY/giphy.gif",
+        "https://media.giphy.com/media/l2JdUKyOBy2r2FhXa/giphy.gif"
     ]
 }
